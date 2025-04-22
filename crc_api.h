12916730862abbd19bb1d88425bfa669776469dd
Joshua Estes <estesjn2020@gmail.com>
2025-04-22 13:30:51 -0400

    Added datasheet

--- crc_api.h
+++
@@ -1,9 +0,0 @@
-#include "msp430.h"
-
-void init_crc16(uint16_t seed);
-void calc_crc16_ba_nondma(uint8_t* data);
-void calc_crc16rb_ba_nondma(uint8_t* data);
-void calc_crc16_ba_dma(uint8_t* data);
-void calc_crc16rb_ba_dma(uint8_t* data);
-void calc_crc16_b(uint8_t dma);
-void calc_crc16rb_b(uint8_t dma);
--- i2c_api.h
+++
@@ -1,9 +0,0 @@
-struct udevice {
-    
-} udevice;
-
-void i2c_init();
-void i2c_write_byte();
-void i2c_write_bytes();
-void i2c_read_byte();
-void i2c_read_bytes();
